1fa8df552c7b01bc1e6210a129d4697a4eca04e2
madalina.toma <madalina.toma@softem.ro>
2022-12-11 13:56:32 +0200

    Add project files.

---
+++ lab3ex1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace lab3ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Ex 1  Scrieti o functie care va calcula suma cifrelor unui numar.
+             */
+            Console.WriteLine("Introduceti numarul: ");
+            int numar = int.Parse(Console.ReadLine());
+            int suma = SumaCifrelor(numar);
+            Console.WriteLine($"Suma cifrelor numarului {numar} este: {suma}");
+
+            static int SumaCifrelor(int n) {
+                var sum = 0;
+                while (n > 0)
+                {
+                    sum = sum + (n % 10);
+                    n = n / 10;
+                }
+                return sum;
+            }
+        }
+    }
+}
